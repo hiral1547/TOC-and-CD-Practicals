b5ba5cd6f0e6c76937a10f6afc1a4f27a9775d52
Hiral Patel <hiral1547@gmail.com>
2026-08-16 14:33:39 +0000

    Practical 4

---
+++ cd_practicals/input.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+// Variable declaration
+int a = 10, b = 20;
+
+/*
+   This is a
+   multi line comment
+   explaining the program
+*/
+
+int main()
+{
+    // Addition
+    int sum = a + b;
+
+    printf("Sum = %d\n", sum); // Print result
+
+    /* End of main function */
+
+    return 0; // End of program
+    // Yes! It's an End.
+}
